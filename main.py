c5d6818829e5bc3786b3aac8a0bef1a98e6909d7
vishwas-mehta <vishwas.mehta.work@gmail.com>
2026-01-11 22:30:19 +0530

    Learning fastApi

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    description: str
 
 @app.get('/')
 def main():
@@ -9,3 +15,8 @@ def main():
 @app.post('/')  
 def main():
     return 'Hello World'
+
+@app.put('/')
+def main():
+    return 'This is put request'
+
